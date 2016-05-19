fb3ee789ed2146b1cce0d48fe209b4d517d3194b
Paul Chamberlain <pchamberlain@bluestatedigital.com>
2016-05-19 18:41:04 -0400

    Finish root item types list

--- app/components/RootItemTypes.js
+++ app/components/RootItemTypes.js
@@ -1,14 +1,23 @@
 var React = require('react');
+var PropTypes = React.PropTypes;
+var capitalize = require('../styles').capitalize;
+
 
 var RootItemTypes = React.createClass({
     render: function () {
         return (
-            <div>
-                <li className="list-group-item">Planets</li>
-                <li className="list-group-item">Spaceships</li>
-            </div>
+            <ul>
+            {this.props.rootItemTypes.map(function(rootItem, index) {
+                return <li className="list-group-item" style={capitalize} key={index}>{rootItem}</li>;
+            })}
+            </ul>
         )
     }
 });
+
+RootItemTypes.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+  rootItemTypes: PropTypes.array.isRequired,
+}
 
 module.exports = RootItemTypes;
